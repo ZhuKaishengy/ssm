5828c9e30de278cd1dd8dc1a55e02abbf2e2877b
ZhuKaishengy <13109894018@163>
2017-07-20 17:35:24 +0800

    create project

---
+++ SSM/src/main/java/com/aisino/domain/User.java
@@ -0,0 +1,29 @@
+package com.aisino.domain;
+
+public class User {
+
+	private int userId;
+	private String username;
+	private int password;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	
+	
+}
